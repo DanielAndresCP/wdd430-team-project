b99d570bdf513e54884bf55e285abf97dd0dedf9
Daniel Concha <danielconchaparra@gmail.com>
2025-04-10 19:12:38 -0400

    Use SettingsNavBar component on settings layout, and add base logic for dynamic rendereng of nav items

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,51 +1,41 @@
-"use client";
-
-import clsx from "clsx";
-import Link from "next/link";
-import React from "react";
-import { usePathname } from "next/navigation";
+import SettingsNavBar from "@/components/settingsNavBar";
 
 const settingsNavLinks = [
   {
     text: "Account Settings",
     href: "/settings/account",
+    allowedRoles: ["USER","SELLER", "ADMIN"],
   },
   {
     text: "Seller Profile Settings",
     href: "/settings/seller",
+    allowedRoles: ["SELLER", "ADMIN"],
   },
   {
     text: "My Products",
     href: "/settings/products",
+    allowedRoles: ["SELLER", "ADMIN"],
   },
   {
     text: "Reviews to my products",
     href: "/settings/reviews",
+    allowedRoles: ["SELLER", "ADMIN"],
   },
 ];
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  const currentPath = usePathname();
+// TODO, make the shown links depend on the user role
+const currentRole = "ADMIN";
+const filteredNavLinks = settingsNavLinks
+  .filter((x) => x.allowedRoles.includes(currentRole))
+  .map((x) => ({ text: x.text, href: x.href }));
 
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-title mb-4">Settings</h1>
       <div className="grid grid-cols-1 sm:grid-cols-[1fr_2fr] gap-12">
         <aside className="p-6 bg-green-desaturated-light rounded-xl max-h-fit">
-          <nav className="flex flex-col gap-2">
-            {settingsNavLinks.map((x, i, arr) => (
-              <Link
-                href={x.href}
-                key={x.href}
-                className={clsx("block", {
-                  "pl-2 border-l-4 border-green-900":
-                    currentPath === x.href,
-                })}
-              >
-                {x.text}
-              </Link>
-            ))}
-          </nav>
+          <SettingsNavBar settingsNavLinks={filteredNavLinks} />
         </aside>
         {children}
       </div>
