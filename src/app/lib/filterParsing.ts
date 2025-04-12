9f0c432c5b7de84d1c8e2d3f0ad855567c19ea5c
Daniel Concha <danielconchaparra@gmail.com>
2025-04-11 21:06:02 -0400

    Created fuseFilters function for use with prisma

---
+++ src/app/lib/filterParsing.ts
@@ -0,0 +1,19 @@
+export function fuseFilters(...args: Array<string | object>) {
+  const parsedFilters = [];
+
+  for (const item of args) {
+    if (typeof item === "string") {
+      try {
+        parsedFilters.push(JSON.parse(item));
+      } catch (error) {
+        continue;
+      }
+    }
+
+    if (typeof item === "object") {
+      parsedFilters.push(item);
+    }
+  }
+
+  return parsedFilters.reduce((acc, obj) => ({ ...acc, ...obj }), {});
+}
