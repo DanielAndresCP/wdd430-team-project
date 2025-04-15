6e0a6bce5821d351f48439424ffa3dab2c07bc47
Daniel Concha <danielconchaparra@gmail.com>
2025-04-14 21:31:39 -0400

    Add fetchProductPages action

--- src/app/lib/product-actions.ts
+++ src/app/lib/product-actions.ts
@@ -2,9 +2,9 @@ import { PrismaClient } from "@prisma/client";
 import { fuseFilters } from "./filterParsing";
 
 const prisma = new PrismaClient();
+const PRODUCTS_PER_PAGE = 10;
 
 export async function fetchProducts(query: string, page: number) {
-  const PRODUCTS_PER_PAGE = 10;
   const offset = (page - 1) * PRODUCTS_PER_PAGE;
 
   const options: { skip: number; take: number; where: Object } = {
@@ -16,4 +16,18 @@ export async function fetchProducts(query: string, page: number) {
   const result = await prisma.product.findMany(options);
 
   return result;
+}
+
+export async function fetchProductPages(query: string) {
+  const options = {
+    where: fuseFilters(query),
+  };
+
+  
+  const result = await prisma.product.count(options);
+
+  return {
+    totalPages: Math.ceil(result / PRODUCTS_PER_PAGE),
+    totalAmount: result,
+  };
 }
